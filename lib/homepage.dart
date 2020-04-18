b239ac51c33d13bab263004b4133eb667c1e9b22
Joris TRUONG <joris.truong@protonmail.com>
2020-04-18 19:52:12 +0200

    feat: added drawer

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,12 +11,57 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  List options = [
+    {'name': 'Home', 'icon': Icons.home},
+    {'name': 'Categories', 'icon': Icons.category},
+    {'name': 'Graphs', 'icon': Icons.pie_chart},
+    {'name': 'Analyze', 'icon': Icons.show_chart},
+    {'name': 'About us', 'icon': Icons.info_outline}
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            fontSize: 24
+          ),
+        ),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0.0),
+          children: <Widget>[
+            DrawerHeader(
+              child: null,
+              decoration: BoxDecoration(
+                color: Colors.grey
+              ),
+            ),
+            ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: options.length,
+              itemBuilder: (BuildContext context, int index) {
+                Map item = options[index];
+                return ListTile(
+                  leading: Icon(item['icon']),
+                  title: Text(
+                    item['name'],
+                    style: TextStyle(
+                      fontSize: 20,
+                    )
+                  ),
+                  onTap: () {
+                    print('${item['name']} pressed');
+                  },
+                );
+              },
+            )
+          ]
         )
       ),
       body: Center(
