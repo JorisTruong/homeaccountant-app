a412e37cb1cf2800099c90d6a857e25dacc83d1e
Joris TRUONG <joris.truong@protonmail.com>
2020-05-14 15:42:17 +0200

    feat: gesture detector for speed dial in transactions page

--- lib/screen/transactions.dart
+++ lib/screen/transactions.dart
@@ -112,84 +112,91 @@ class _TransactionsPageState extends State<TransactionsPage> with TickerProvider
             print(StoreProvider.of<AppState>(context).state);
             return new Future(() => true);
           },
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text(
-                'Transactions',
-                style: TextStyle(
-                    fontSize: baseFontSize.title
+          child: GestureDetector(
+            onTap: () {
+              if (!_controller.isDismissed) {
+                _controller.reverse();
+              }
+            },
+            child: Scaffold(
+              appBar: AppBar(
+                title: Text(
+                  'Transactions',
+                  style: TextStyle(
+                      fontSize: baseFontSize.title
+                  ),
                 ),
+                centerTitle: true,
+                actions: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: FloatingActionButton(
+                      elevation: 0,
+                      backgroundColor: baseColors.transparent,
+                      onPressed: () {
+                        if (_controller.isDismissed) {
+                          _controller.forward();
+                        } else {
+                          _controller.reverse();
+                        }
+                      },
+                      child: new AnimatedBuilder(
+                        animation: _controller,
+                        builder: (BuildContext context, Widget child) {
+                          return new Icon(Icons.more_vert);
+                        }
+                      )
+                    )
+                  )
+                ],
               ),
-              centerTitle: true,
-              actions: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: FloatingActionButton(
-                    elevation: 0,
-                    backgroundColor: baseColors.transparent,
-                    onPressed: () {
-                      if (_controller.isDismissed) {
-                        _controller.forward();
-                      } else {
-                        _controller.reverse();
-                      }
-                    },
-                    child: new AnimatedBuilder(
-                      animation: _controller,
-                      builder: (BuildContext context, Widget child) {
-                        return new Icon(Icons.more_vert);
-                      }
+              drawer: NavigationDrawer(),
+              body: SingleChildScrollView(
+                child: Stack(
+                  children: [
+                    Column(
+                      children: List.generate(transactions.length, (int index) {
+                        var month = transactions.keys.elementAt(index);
+                        return TransactionCard(month, transactions[month]);
+                      })
                     )
+                  ]..addAll(
+                    [
+                      SpeedDialButton(_controller, _pcAccount, _pcDate),
+                      SlidingUpPanel(
+                        controller: _pcAccount,
+                        panel: Center(child: Text("This is the sliding Widget for Account"),),
+                        backdropEnabled: true,
+                        minHeight: 0.0,
+                        maxHeight: 0.8 * MediaQuery.of(context).size.height,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(24.0),
+                          topRight: Radius.circular(24.0)
+                        ),
+                      ),
+                      SlidingUpPanel(
+                        controller: _pcDate,
+                        panel: Center(child: Text("This is the sliding Widget for Date Range"),),
+                        backdropEnabled: true,
+                        minHeight: 0.0,
+                        maxHeight: 0.8 * MediaQuery.of(context).size.height,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(24.0),
+                          topRight: Radius.circular(24.0)
+                        ),
+                      )
+                    ]
                   )
                 )
-              ],
-            ),
-            drawer: NavigationDrawer(),
-            body: SingleChildScrollView(
-              child: Stack(
-                children: [
-                  Column(
-                    children: List.generate(transactions.length, (int index) {
-                      var month = transactions.keys.elementAt(index);
-                      return TransactionCard(month, transactions[month]);
-                    })
-                  )
-                ]..addAll(
-                  [
-                    SpeedDialButton(_controller, _pcAccount, _pcDate),
-                    SlidingUpPanel(
-                      controller: _pcAccount,
-                      panel: Center(child: Text("This is the sliding Widget for Account"),),
-                      backdropEnabled: true,
-                      minHeight: 0.0,
-                      maxHeight: 0.8 * MediaQuery.of(context).size.height,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(24.0),
-                        topRight: Radius.circular(24.0)
-                      ),
-                    ),
-                    SlidingUpPanel(
-                      controller: _pcDate,
-                      panel: Center(child: Text("This is the sliding Widget for Date Range"),),
-                      backdropEnabled: true,
-                      minHeight: 0.0,
-                      maxHeight: 0.8 * MediaQuery.of(context).size.height,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(24.0),
-                        topRight: Radius.circular(24.0)
-                      ),
-                    )
-                  ]
-                )
-              )
-            ),
-            floatingActionButton: FloatingActionButton(
-              heroTag: null,
-              onPressed: () {
-                StoreProvider.of<AppState>(context).dispatch(NavigatePushAction(AppRoutes.transaction));
-              },
-              child: Icon(Icons.add),
-            ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                heroTag: null,
+                onPressed: () {
+                  StoreProvider.of<AppState>(context).dispatch(NavigatePushAction(AppRoutes.transaction));
+                },
+                child: Icon(Icons.add),
+              ),
+            )
           )
         );
       }
