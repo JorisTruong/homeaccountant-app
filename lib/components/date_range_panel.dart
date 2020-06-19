49c7ccde03f8442cffc61c6d0a7c0cfe99d29111
Joris TRUONG <joris.truong@protonmail.com>
2020-06-19 17:04:11 +0200

    fix: ability to scroll date panel

--- lib/components/date_range_panel.dart
+++ lib/components/date_range_panel.dart
@@ -39,149 +39,151 @@ class _DateRangePanelState extends State<DateRangePanel> with TickerProviderStat
     return StoreConnector<AppState, List<String>>(
       converter: (Store<AppState> store) => store.state.route,
       builder: (BuildContext context, List<String> route) {
-        return Padding(
-          padding: EdgeInsets.only(top: 50, left: 20, right: 20, bottom: 50),
-          child: Column(
-            children: [
-              /// Dropdown to select the date range type
-              DropdownButtonHideUnderline(
-                child: ButtonTheme(
-                  alignedDropdown: true,
-                  child: Card(
-                    margin: EdgeInsets.only(bottom: 30),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40.0),
-                      side: BorderSide(color: baseColors.borderColor)
-                    ),
-                    child: Row(
-                      children: [
-                        SizedBox(width: 15.0),
-                        Icon(Icons.timeline, size: 18.0),
-                        Expanded(
-                          child: DropdownButton<String>(
-                            autofocus: true,
-                            icon: Icon(Icons.keyboard_arrow_down),
-                            value: _store.state.dateRangeType,
-                            hint: Text(
-                              'Type',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: baseColors.secondaryColor)
+        return SingleChildScrollView(
+          child: Padding (
+            padding: EdgeInsets.only(top: 50, left: 20, right: 20, bottom: 50),
+            child: Column(
+              children: [
+                /// Dropdown to select the date range type
+                DropdownButtonHideUnderline(
+                  child: ButtonTheme(
+                    alignedDropdown: true,
+                    child: Card(
+                      margin: EdgeInsets.only(bottom: 30),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                        side: BorderSide(color: baseColors.borderColor)
+                      ),
+                      child: Row(
+                        children: [
+                          SizedBox(width: 15.0),
+                          Icon(Icons.timeline, size: 18.0),
+                          Expanded(
+                            child: DropdownButton<String>(
+                              autofocus: true,
+                              icon: Icon(Icons.keyboard_arrow_down),
+                              value: _store.state.dateRangeType,
+                              hint: Text(
+                                'Type',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: baseColors.secondaryColor)
+                              ),
+                              isDense: false,
+                              onTap: () {
+                                if (!currentFocus.hasPrimaryFocus) {
+                                  currentFocus.unfocus();
+                                }
+                              },
+                              onChanged: (String newValue) {
+                                _store.dispatch(UpdateDateRangeType(newValue));
+                              },
+                              items: dateRangeTypes.map((key) {
+                                return DropdownMenuItem<String>(
+                                  value: key,
+                                  child: Text(key),
+                                );
+                              }).toList(),
                             ),
-                            isDense: false,
-                            onTap: () {
-                              if (!currentFocus.hasPrimaryFocus) {
-                                currentFocus.unfocus();
-                              }
-                            },
-                            onChanged: (String newValue) {
-                              _store.dispatch(UpdateDateRangeType(newValue));
-                            },
-                            items: dateRangeTypes.map((key) {
-                              return DropdownMenuItem<String>(
-                                value: key,
-                                child: Text(key),
-                              );
-                            }).toList(),
                           ),
-                        ),
-                        SizedBox(width: 15.0)
-                      ],
-                    )
-                  )
-                )
-              ),
-              /// Conditional rendering of a calendar, depending on the range type
-              if (_store.state.dateRangeType == 'Year')
-                Material(
-                  color: baseColors.transparent,
-                  child: Container(
-                    padding: EdgeInsets.only(bottom: 20),
-                    height: MediaQuery.of(context).size.height * 0.3,
-                    width: MediaQuery.of(context).size.width,
-                    child: yp.YearPicker(
-                      selectedDate: _store.state.selectedDate,
-                      firstDate: DateTime(1900),
-                      lastDate: DateTime.now(),
-                      onChanged: (datePeriod) {
-                        _store.dispatch(UpdateSelectedDate(datePeriod));
-                      },
-                    )
-                  )
-                ),
-              if (_store.state.dateRangeType == 'Month')
-                dp.MonthPicker(
-                  selectedDate: _store.state.selectedDate,
-                  firstDate: DateTime(1900),
-                  lastDate: DateTime(2100),
-                  onChanged: (datePeriod) {
-                    _store.dispatch(UpdateSelectedDate(datePeriod));
-                  },
-                ),
-              if (_store.state.dateRangeType == 'Week')
-                dp.WeekPicker(
-                  selectedDate: _store.state.selectedDate,
-                  firstDate: DateTime(1900),
-                  lastDate: DateTime(2100),
-                  onChanged: (datePeriod) {
-                    _store.dispatch(UpdateSelectedDate(datePeriod.start));
-                  },
-                ),
-              /// Validate and cancel the operation
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  RaisedButton(
-                    onPressed: () {
-                      widget._pcDate.close();
-                    },
-                    child: Text(
-                      'CANCEL',
-                      style: TextStyle(
-                        fontSize: baseFontSize.text,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white
+                          SizedBox(width: 15.0)
+                        ],
                       )
-                    ),
-                    color: baseColors.red,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40.0),
-                    ),
+                    )
+                  )
+                ),
+                /// Conditional rendering of a calendar, depending on the range type
+                if (_store.state.dateRangeType == 'Year')
+                  Material(
+                    color: baseColors.transparent,
+                    child: Container(
+                      padding: EdgeInsets.only(bottom: 20),
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      width: MediaQuery.of(context).size.width,
+                      child: yp.YearPicker(
+                        selectedDate: _store.state.selectedDate,
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime.now(),
+                        onChanged: (datePeriod) {
+                          _store.dispatch(UpdateSelectedDate(datePeriod));
+                        },
+                      )
+                    )
                   ),
-                  SizedBox(width: 12.0),
-                  RaisedButton(
-                    onPressed: () {
-                      print(
-                        dateToDateRange(
-                          _store.state.dateRangeType,
-                          _store.state.selectedDate
+                if (_store.state.dateRangeType == 'Month')
+                  dp.MonthPicker(
+                    selectedDate: _store.state.selectedDate,
+                    firstDate: DateTime(1900),
+                    lastDate: DateTime(2100),
+                    onChanged: (datePeriod) {
+                      _store.dispatch(UpdateSelectedDate(datePeriod));
+                    },
+                  ),
+                if (_store.state.dateRangeType == 'Week')
+                  dp.WeekPicker(
+                    selectedDate: _store.state.selectedDate,
+                    firstDate: DateTime(1900),
+                    lastDate: DateTime(2100),
+                    onChanged: (datePeriod) {
+                      _store.dispatch(UpdateSelectedDate(datePeriod.start));
+                    },
+                  ),
+                /// Validate and cancel the operation
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    RaisedButton(
+                      onPressed: () {
+                        widget._pcDate.close();
+                      },
+                      child: Text(
+                        'CANCEL',
+                        style: TextStyle(
+                          fontSize: baseFontSize.text,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white
                         )
-                      );
-                      _store.dispatch(
-                        UpdateDateRange(
+                      ),
+                      color: baseColors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                      ),
+                    ),
+                    SizedBox(width: 12.0),
+                    RaisedButton(
+                      onPressed: () {
+                        print(
                           dateToDateRange(
                             _store.state.dateRangeType,
                             _store.state.selectedDate
                           )
+                        );
+                        _store.dispatch(
+                          UpdateDateRange(
+                            dateToDateRange(
+                              _store.state.dateRangeType,
+                              _store.state.selectedDate
+                            )
+                          )
+                        );
+                        widget._pcDate.close();
+                      },
+                      child: Text(
+                        'VALIDATE',
+                        style: TextStyle(
+                          fontSize: baseFontSize.text,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white
                         )
-                      );
-                      widget._pcDate.close();
-                    },
-                    child: Text(
-                      'VALIDATE',
-                      style: TextStyle(
-                        fontSize: baseFontSize.text,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white
-                      )
+                      ),
+                      color: baseColors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                      ),
                     ),
-                    color: baseColors.green,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40.0),
-                    ),
-                  ),
-                ],
-              )
-            ]
+                  ],
+                )
+              ]
+            )
           )
         );
       }
