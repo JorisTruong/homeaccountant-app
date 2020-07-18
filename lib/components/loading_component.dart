298ad13977d94be5efcd8b2fb261d77c875fa28a
Joris TRUONG <joris.truong@protonmail.com>
2020-07-18 17:37:54 +0200

    feat: added size parameter to loading component

--- lib/components/loading_component.dart
+++ lib/components/loading_component.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 class LoadingComponent extends StatelessWidget {
+  final double size;
+
+  LoadingComponent({this.size = 20});
+
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Padding(
-          padding: EdgeInsets.all(20),
-          child: CircularProgressIndicator(),
+          padding: EdgeInsets.all(size),
+          child: SizedBox(
+            child: CircularProgressIndicator(),
+            height: size,
+            width: size
+          )
         )
       ]
     );
